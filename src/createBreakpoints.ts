7ea7b720365979ce8bf2d4255604967a70c29b45
Sergey Baleyko <seryozha.baleyko@gmail.com>
2022-04-08 00:10:13 +0300

    init

--- src/createBreakpoints.ts
+++ src/createBreakpoints.ts
@@ -1,84 +1,70 @@
-import {Breakpoint, Breakpoints, BreakpointsOptions} from './types';
-
-// Sorted ASC by size. That's important.
-// It can't be configured as it's used statically for propTypes.
-export const breakpointKeys = ['xs', 'sm', 'md', 'lg', 'xl'];
+import { Options, Breakpoint, Breakpoints } from './types';
 
 const sortBreakpointsValues = (values: { [key in Breakpoint]: number }) => {
-    const breakpointsAsArray = Object.keys(values).map((key) => ({ key, val: values[key] })) || [];
-    // Sort in ascending order
+    const breakpointsAsArray = (Object.keys(values) as (keyof typeof values)[]).map((key) => ({
+        key,
+        val: values[key]
+    })) || [];
+
     breakpointsAsArray.sort((breakpoint1, breakpoint2) => breakpoint1.val - breakpoint2.val);
-    return breakpointsAsArray.reduce((acc, obj) => {
-        return { ...acc, [obj.key]: obj.val };
-    }, {});
+
+    return breakpointsAsArray.reduce((acc, obj) => ({
+        ...acc,
+        [obj.key]: obj.val
+    }), {}) as { [key in Breakpoint]: number };
 };
 
-// Keep in mind that @media is inclusive by the CSS specification.
-export default function createBreakpoints(breakpoints: BreakpointsOptions): Breakpoints {
+export function createBreakpoints(options: Options): Breakpoints {
     const {
-        // The breakpoint **start** at this value.
-        // For instance with the first breakpoint xs: [xs, sm).
         values = {
-            xs: 0, // phone
-            sm: 600, // tablet
-            md: 900, // small laptop
-            lg: 1200, // desktop
-            xl: 1536, // large screen
+            xs: 0,
+            sm: 600,
+            md: 900,
+            lg: 1200,
+            xl: 1536
         },
         unit = 'px',
-        step = 5,
-        ...other
-    } = breakpoints;
+        step = 5
+    } = options;
 
     const sortedValues = sortBreakpointsValues(values);
-    const keys = Object.keys(sortedValues) as  Breakpoint[];
+    const keys = Object.keys(sortedValues) as Breakpoint[];
 
-    function up(key: Breakpoint | number): string {
-        const value = typeof values[key] === 'number' ? values[key] : key;
-        return `@media (min-width:${value}${unit})`;
-    }
+    const up = (key: Breakpoint) => {
+        return `@media (min-width:${values[key]}${unit})`;
+    };
 
-    function down(key: Breakpoint | number): string {
-        const value = typeof values[key] === 'number' ? values[key] : key;
-        return `@media (max-width:${value - step / 100}${unit})`;
-    }
+    const down = (key: Breakpoint) => {
+        return `@media (max-width:${values[key] - step / 100}${unit})`;
+    };
 
-    function between(start: Breakpoint | number, end: Breakpoint | number): string {
+    const between = (start: Breakpoint, end: Breakpoint) => {
         const endIndex = keys.indexOf(end);
 
-        return (
-            `@media (min-width:${
-                typeof values[start] === 'number' ? values[start] : start
-            }${unit}) and ` +
-            `(max-width:${
-                (endIndex !== -1 && typeof values[keys[endIndex]] === 'number'
-                    ? values[keys[endIndex]]
-                    : end) -
-                step / 100
-            }${unit})`
-        );
-    }
+        return `@media (min-width:${values[start]}${unit}) and (max-width:${values[keys[endIndex]] - step / 100}${unit})`;
+    };
 
-    function only(key: Breakpoint): string {
+    const only = (key: Breakpoint) => {
         if (keys.indexOf(key) + 1 < keys.length) {
             return between(key, keys[keys.indexOf(key) + 1]);
         }
 
         return up(key);
-    }
+    };
 
-    function not(key: Breakpoint): string {
-        // handle first and last key separately, for better readability
+    const not = (key: Breakpoint) => {
         const keyIndex = keys.indexOf(key);
+
         if (keyIndex === 0) {
             return up(keys[1]);
         }
+
         if (keyIndex === keys.length - 1) {
             return down(keys[keyIndex]);
         }
 
         return between(key, keys[keys.indexOf(key) + 1]).replace('@media', '@media not all and');
-    }
+    };
 
     return {
         keys,
@@ -87,8 +73,6 @@ export default function createBreakpoints(breakpoints: BreakpointsOptions): Brea
         down,
         between,
         only,
-        not,
-        unit,
-        ...other,
+        not
     };
 }
